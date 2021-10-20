ea9ab7012fcdca14a0716f7e3e2c1c08f628c263
kelabdi <ingeniero.karim@gmail.com>
2021-10-20 21:16:51 +0200

    Added python scripts to predict videos WIP

--- pred_video.py
+++ pred_video.py
@@ -1,10 +1,30 @@
 import cv2
 import time
+from utils.config import CarPredictionConfig, MaskRCNN
+from utils.functions import adapt_image, plot_video_pred
 
 
+# Loading prediction model
+predconfig = CarPredictionConfig()
+model = MaskRCNN(mode="inference", model_dir="model/pred/", config=predconfig)
+model.load_weights("model/mask_rcnn_car_0009.h5", by_name=True)
+
+
+# Spliting the video into frames
 cap = cv2.VideoCapture("videos/ferrari.mp4")
+ptime = 0
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    img = cv2.resize(img, (960,540))
+    sample = adapt_image(img, predconfig)
+    
+    # Put FPS Counter
+    ctime = time.time()
+    fps = 1/(ctime - ptime)
+    ptime = ctime
+    cv2.putText(sample, f"FPS: {int(fps)}", (850,50), cv2.FONT_HERSHEY_PLAIN, 
+            1.5, (0,255,0),2)
+    # ploting prediction
+    plot_video_pred(sample, model)
+    
---
+++ utils/config.py
@@ -0,0 +1,17 @@
+from os import listdir
+import pandas as pd
+import numpy as np
+
+from mrcnn.utils import Dataset, extract_bboxes
+from mrcnn.visualize import display_instances
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+
+
+
+
+class CarPredictionConfig(Config):
+    NAME = "car_cfg"
+    NUM_CLASSES = 2
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
