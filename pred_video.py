06af0974c1f40b1fa46540e4ffdab0d040b4c507
kelabdi <ingeniero.karim@gmail.com>
2021-10-21 21:06:09 +0200

    The pred works, but need to reestructure the code WIP

--- pred_video.py
+++ pred_video.py
@@ -2,29 +2,45 @@ import cv2
 import time
 from utils.config import CarPredictionConfig, MaskRCNN
 from utils.functions import adapt_image, plot_video_pred
+import warnings
+warnings.filterwarnings("ignore")
 
-
-# Loading prediction model
-predconfig = CarPredictionConfig()
-model = MaskRCNN(mode="inference", model_dir="model/pred/", config=predconfig)
-model.load_weights("model/mask_rcnn_car_0009.h5", by_name=True)
-
-
-# Spliting the video into frames
-cap = cv2.VideoCapture("videos/ferrari.mp4")
 ptime = 0
 
-while True:
-    success, img = cap.read()
-    img = cv2.resize(img, (960,540))
-    sample = adapt_image(img, predconfig)
-    
-    # Put FPS Counter
-    ctime = time.time()
-    fps = 1/(ctime - ptime)
-    ptime = ctime
-    cv2.putText(sample, f"FPS: {int(fps)}", (850,50), cv2.FONT_HERSHEY_PLAIN, 
-            1.5, (0,255,0),2)
-    # ploting prediction
-    plot_video_pred(sample, model)
-    
+filename = "ferrari.mp4"
+
+def save_video(filename):
+    # Spliting the video into frames
+    cap = cv2.VideoCapture("videos/" + filename)
+    i = 0
+    while True:
+        success, img = cap.read()
+        img = cv2.resize(img, (960,540))
+		cv2.imwrite("prediction/" + filename + f"/{i}.jpg", img)
+		i+=1
+        
+
+		# AQUI HAY QUE MONTAR UNA FUNCION BONITA
+        # Put FPS Counter
+        ctime = time.time()
+        fps = 1/(ctime - ptime)
+        ptime = ctime
+        cv2.putText(img, f"FPS: {int(fps)}", (850,50), cv2.FONT_HERSHEY_PLAIN, 
+                1.5, (0,255,0),2)
+        # ploting prediction
+        plot_video_pred(img, sample, model)
+        cv2.imshow("Prediction", img)
+        cv2.waitKey(1)
+
+def pred_(path):
+	# Loading prediction model
+	predconfig = CarPredictionConfig()
+	model = MaskRCNN(mode="inference", model_dir="model/pred/", config=predconfig)
+	model.load_weights("model/mask_rcnn_car_0009.h5", by_name=True)
+	
+	sample = adapt_image(img, predconfig)
+
+	for filename in listdir(path):
+        image_id = filename
+
+
--- utils/functions.py
+++ utils/functions.py
@@ -53,15 +53,12 @@ def plot_actual_vs_predicted(dataset, model, cfg, n_images=5):
 	plt.show()
 
 
-def plot_video_pred(sample, model):
-    yhat = model.detect(sample, verbose=0)[0]
-    # plot each box
-	for box in yhat['rois']:
-        # get coordinates
-		y1, x1, y2, x2 = box
-        # create the shape
-		cv2.rectangle((x1, y1), (x2, y2), (255,0,255), 2)
-    # show the figure
-	cv2.imshow("Prediction", sample)
-    cv2.waitKey(20)
-    
+def plot_video_pred(img, sample, model):
+	yhat = model.detect(sample, verbose=0)[0]
+	# plot each box
+	try:
+		for box in yhat['rois']:
+			y1, x1, y2, x2 = box
+			cv2.rectangle(img,(x1, y1), (x2, y2), (255,0,255), 2)
+	except:
+		pass
